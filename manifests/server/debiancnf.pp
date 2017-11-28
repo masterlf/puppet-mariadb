8472f87ca5da01abea50a9ae18cc80277b81a4fc
Frederic Lauret <frederic.lauret@emerchantpay.com>
2017-11-28 15:58:26 +0200

    bug fix on username

--- manifests/server/debiancnf.pp
+++ manifests/server/debiancnf.pp
@@ -1,4 +1,9 @@
-class mariadb::server::debiancnf {
+# this class set a specific password for mysql debian service
+#
+class mariadb::server::debiancnf (
+  $package_names           = $mariadb::params::server_package_names,
+  $debiansysmaint_password = undef,
+  ) inherits mariadb::params {
   if $::osfamily == 'Debian' and $debiansysmaint_password != undef {
     file { '/etc/mysql/debian.cnf':
       content => template('mariadb/debian.cnf.erb'),
